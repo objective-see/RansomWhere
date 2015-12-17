f0895bbd9bfd5b9e92c4feb9adfa1551c4359317
Patrick Wardle <patrick@objective-see.com>
2015-12-16 22:16:44 -1000

    updated kext to use KAUTH_SCOPE_VNODE instead of KAUTH_SCOPE_FILEOP  ->allows procs that fork to be tracked more accurately :)

--- BlockBlockKext/BlockBlockKext.c
+++ BlockBlockKext/BlockBlockKext.c
@@ -10,8 +10,13 @@
 
 #include <sys/proc.h>
 #include <sys/kauth.h>
+#include <sys/vnode.h>
 #include <sys/kern_event.h>
 #include <libkern/libkern.h>
+
+//apple does this
+// ->see proc_internal.h
+#define PROC_NULL (struct proc *)0
 
 /* FUNCTIONS */
 
@@ -25,6 +30,10 @@ kern_return_t stop(kmod_info_t *ki, void *d);
 
 //kauth callback
 // ->for KAUTH_FILEOP_EXEC events, broadcast process notifications to user-mode
+//static int processExec(kauth_cred_t credential, void* idata, kauth_action_t action, uintptr_t arg0, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3);
+
+//kauth callback
+// ->for KAUTH_SCOPE_VNODE events, broadcast process notifications to user-mode
 static int processExec(kauth_cred_t credential, void* idata, kauth_action_t action, uintptr_t arg0, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3);
 
 /* GLOBALS */
@@ -37,7 +46,7 @@ kauth_listener_t kauthListener = NULL;
 u_int32_t objSeeVendorID = 0;
 
 /* CODE */
-
+/*
 //kauth callback
 // ->for KAUTH_FILEOP_EXEC events, broadcast process notifications to user-mode
 static int processExec(kauth_cred_t credential, void* idata, kauth_action_t action, uintptr_t arg0, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3)
@@ -160,6 +169,163 @@ bail:
     
     return KAUTH_RESULT_DEFER;
 }
+*/
+
+
+//kauth callback
+// ->for KAUTH_SCOPE_VNODE events, broadcast process notifications to user-mode
+static int processExec(kauth_cred_t credential, void* idata, kauth_action_t action, uintptr_t arg0, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3)
+{
+    //status var
+    errno_t status = KERN_FAILURE;
+    
+    //chunk pointer
+    char* chunkPointer = NULL;
+    
+    //type
+    enum vtype type = {0};
+    
+    //path length
+    int pathLength = MAXPATHLEN;
+    
+    //path
+    char path[MAXPATHLEN+1] = {0};
+    
+    //uid
+    uid_t uid = -1;
+    
+    //pid
+    int pid = -1;
+    
+    //proc
+    proc_t proc = PROC_NULL;
+    
+    //ppid
+    pid_t ppid = -1;
+    
+    //kernel event message
+    struct kev_msg kEventMsg = {0};
+    
+    //non-path size
+    int nonPathSize = 0;
+    
+    //get vnode type
+    type = vnode_vtype((vnode_t)arg1);
+    
+    //ignore all non exec events
+    if( (KAUTH_VNODE_EXECUTE != action) ||
+        (type & VDIR) )
+    {
+        //bail
+        goto bail;
+    }
+    
+    //zero out path
+    bzero(&path, sizeof(path));
+    
+    //get path
+    vn_getpath((vnode_t)arg1, path, &pathLength);
+
+    //get pid
+    pid = vfs_context_pid((vfs_context_t)arg0);
+    
+    //get UID
+    uid = kauth_cred_getuid(vfs_context_ucred((vfs_context_t)arg0));
+    
+    //get proc stuct for pid
+    // ->allows us to get parent
+    proc = proc_find(pid);
+    if(PROC_NULL == proc)
+    {
+        //bail
+        goto bail;
+    }
+    
+    //get ppid
+    ppid = proc_ppid(proc);
+    
+    //dbg msg
+    DEBUG_PRINT(("BLOCKBLOCK KEXT: new process: %s %d/%d/%d\n", path, pid, ppid, uid));
+    
+    //calc non-path size
+    // ->pid, uid, and ppid
+    nonPathSize = sizeof(pid_t) + sizeof(uid_t) + sizeof(pid_t);
+    
+    //zero out kernel message
+    bzero(&kEventMsg, sizeof(struct kev_msg));
+    
+    //set vendor code
+    kEventMsg.vendor_code = objSeeVendorID;
+    
+    //set class
+    kEventMsg.kev_class = KEV_ANY_CLASS;
+    
+    //set subclass
+    kEventMsg.kev_subclass = KEV_ANY_SUBCLASS;
+    
+    //set event code
+    kEventMsg.event_code = PROCESS_BEGAN_EVENT;
+    
+    //add pid
+    kEventMsg.dv[0].data_length = sizeof(pid_t);
+    kEventMsg.dv[0].data_ptr = &pid;
+    
+    //add uid
+    kEventMsg.dv[1].data_length = sizeof(uid_t);
+    kEventMsg.dv[1].data_ptr = &uid;
+    
+    //add ppid
+    kEventMsg.dv[2].data_length = sizeof(pid_t);
+    kEventMsg.dv[2].data_ptr = &ppid;
+    
+    //start at path
+    // ->this works, since data before path is small enough to never chunk
+    chunkPointer = path;
+    
+    //send all chunks
+    // ->termination; end of string
+    while(0x0 != *chunkPointer)
+    {
+        //add current offset of path
+        kEventMsg.dv[3].data_ptr = chunkPointer;
+        
+        //set size
+        // ->either string length (with NULL)
+        //   or max size - pid, etc and extra for NULL!
+        kEventMsg.dv[3].data_length = min((u_int)strlen(chunkPointer)+1, (MAX_MSG_SIZE - nonPathSize - 1));
+        
+        //broadcast msg to user-mode
+        status = kev_msg_post(&kEventMsg);
+        if(KERN_SUCCESS != status)
+        {
+            //err msg
+            printf("BLOCKBLOCK KEXT ERROR: kev_msg_post() failed with %d\n", status);
+            
+            //bail
+            goto bail;
+        }
+        
+        //advance chunk pointer
+        chunkPointer += kEventMsg.dv[3].data_length;
+        
+    }//while data to send...
+
+    
+//bail
+bail:
+    
+    //release proc
+    if(PROC_NULL != proc)
+    {
+        //release
+        proc_rele(proc);
+        
+        //unset
+        proc = PROC_NULL;
+    }
+    
+    return KAUTH_RESULT_DEFER;
+}
 
 //start function, automatically invoked
 // ->get vendor code, register KAuth listener, etc
@@ -171,9 +337,23 @@ kern_return_t start(kmod_info_t * ki, void *d)
     //dbg msg
     DEBUG_PRINT(("BLOCKBLOCK KEXT: starting...\n"));
     
+    /*
     //register listener
     // ->scope 'KAUTH_SCOPE_FILEOP'
     kauthListener = kauth_listen_scope(KAUTH_SCOPE_FILEOP, &processExec, NULL);
+    if(NULL == kauthListener)
+    {
+        //err msg
+        printf("BLOCKBLOCK KEXT ERROR: kauth_listen_scope('KAUTH_SCOPE_FILEOP',...) failed\n");
+        
+        //bail
+        goto bail;
+    }
+    */
+    
+    //register listener
+    // ->scope 'KAUTH_SCOPE_VNODE'
+    kauthListener = kauth_listen_scope(KAUTH_SCOPE_VNODE, &processExec, NULL);
     if(NULL == kauthListener)
     {
         //err msg
